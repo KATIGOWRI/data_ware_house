8327470b1ecf6efe54c3e1d501c418f69c0dad39
nagendrakatigowri <nagendrakatigowri143@gmail.com>
2025-05-16 17:09:52 -0400

    Steps to to first step fot dataware house

--- scripts/sql schema.script.sql
+++ scripts/sql schema.script.sql
@@ -1,3 +1,23 @@
+/* 
+--------------------------------------------------------------------------------------------------------
+Create Database And Schemas
+-----------------------------------------------------------------------------------
+Script purpose:
+   Thiws script Creates a new databse named "Datawarehouse' after checking if it already exsistd.
+   If the database exists,it is dropped and recreated.Additionally,The script sets up three schemas within the database:'Bronzze','Sliver','Gold'.
+
+Warning:
+     Running this script will drop the entire "datawarehouse" database if it is exists.
+	 All data in the database will be permanentely deleted.Proceed with dcautio n and ensure youn have proper backuos before running this script.
+*/
+
+
+
+
+
+
+
+
 use [master]
 
 Create database Datawarehouse;
@@ -5,7 +25,7 @@ Create database Datawarehouse;
 use Datawarehouse;
 
 Create Schema Broze;
-Create Schema Sliver;
+Create Schema Scliver;
 Create Schema Gold;
 
 
